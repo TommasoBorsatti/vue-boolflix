459942b1595b04f5db54ce03d91a0da09d0a194d
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-04-09 16:03:24 +0200

    JS - Compilato - problemi nella visualizzazione dei template
    Sono riuscito a far funzionare la chiamata axios e ad acquisire le info richieste dall' API alla pressione di Enter. Non riesco a capire però, sebbene usi v-for e v-bind, perché non le stampi a schermo,

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 
+const App = new Vue ({
+
+  el: "#root",
+
+  data:{
+
+    movies:[],
+    searchQuery:"",
+
+  },
+
+  methods:{
+
+    searchMovie: function () {
+      axios.get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "d49d990ef72ab3ecefdba4305a56dc85",
+          query: this.searchQuery,
+          language: "it-IT"
+        }
+      })
+      .then(function(response){
+        MovieData = response.data.results;
+        this.movies = MovieData;
+        console.log(this.movies);
+      })
+      this.searchQuery = null;
+    },
+
+  },
+
+
+});
