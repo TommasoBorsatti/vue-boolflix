3ac2f2ce94f25d93f1bf149f891fd562e4811e6e
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-04-11 19:48:14 +0200

    Aggiunta la ricerca tra le TV series
    La funzione al click ora produce anche una seconda chiamata AJAX, che salva tutti i risultati delle serie TV in un secondo array. Creo un secondo template, speculare al primo ma con alcune modifiche, e vado a stampare i dati delle serie tv. Adesso la ricerca da prima tutti i risultati dei film fino a 20, e poi tutte le serie tv con la stessa query.

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const App = new Vue ({
   data:{
 
     movies:[],
+    tvSeries:[],
     searchQuery:"",
-    ratings:["", "*", "**", "***", "****", "*****"],
 
   },
 
@@ -26,10 +26,24 @@ const App = new Vue ({
         this.movies = MovieData;
         //dimezzo i voti dei film
         this.movies.forEach(element => element.vote_average = Math.floor(element.vote_average / 2));
+      })
+
+      axios.get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key: "d49d990ef72ab3ecefdba4305a56dc85",
+          query: this.searchQuery,
+          language: "it-IT"
+        }
+      })
+      .then((response)=>{
+        TvData = response.data.results;
+        this.tvSeries = TvData;
+        //dimezzo i voti delle serie
+        this.tvSeries.forEach(element => element.vote_average = Math.floor(element.vote_average / 2));
 
       })
+
       this.searchQuery = "";
-      // arrotondo e dimezzo i voti
 
 
 
